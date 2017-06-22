f50d3f57bed6cc9f820262b760d02df4ccaeb084
unknown <ghotbijr@gmail.com>
2017-06-22 05:10:18 -0700

    added basic weapon classes. Will add spawning weapons with random attributes and picking up weapons next time

---
+++ Assets/Scripts/Game/Weapons/Bow.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : Weapon
+{
+    //add bow specific methods and features
+}
---
+++ Assets/Scripts/Game/Weapons/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//parent class for weapons, quite empty for now but will have info for global weapon stats and useful methods
+public class Weapon {
+    protected float damage;
+    protected float atkSpeed;
+    protected float critChance;
+    protected float critDamage;
+
+
+}
